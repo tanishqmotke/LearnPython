d49d6cfccfed4e801c224f51a9cfba88dd0b99b3
tanishqmotke <tmotke98@gmai.com>
2024-04-04 17:50:09 +0530

    Day 01 learning

---
+++ Input.py
@@ -0,0 +1,17 @@
+
+a = input("Enter your name :")
+b = input("Enter your age: ")
+d = input("Enter no")
+c = "2023 2024"
+
+d= int(c)
+print(type(d))
+print(type(c))
+
+print(a , b)
+print(b + d)
+
+print(int(b)+ int(d))
+
+print(a + b)
+print(int(a) + int(b))
---
+++ hello_chai.py
@@ -0,0 +1,4 @@
+print("Hello World")
+
+def chai(n): 
+    print(n)
