082de9b975798b378c8dcd75554a184065652817
tanishqmotke <tmotke98@gmai.com>
2024-04-06 17:26:28 +0530

    String Slicing

---
+++ StringSlicing.py
@@ -0,0 +1,3 @@
+nm = "Harry"
+print(nm[-4:-2])
+print(nm[len(nm)-4:])
---
+++ Strings.py
@@ -0,0 +1,7 @@
+String1 = '''Hello my name is tanishq
+		I live in nagpur
+		India''' 
+print(String1[0])
+
+for character in String1 :
+    print(character)
